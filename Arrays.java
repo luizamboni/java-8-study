425d8ae67aa0d4593b426f7043fa87a59525857d
luiz.zamboni <luiz.zamboni@MacBook-Pro-de-luizzamboni.local>
2018-12-19 23:40:46 -0200

    INITIAL

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+
+class Main {
+  public static void main(String[] args) {
+
+
+    String[] names = { "luiz", "gerusa" };
+
+    System.out.println("arrays have a fixed size: " + names.length);
+
+    System.out.println("these are the elements:");
+
+    for(String name : names) {
+      System.out.println(name);
+    }
+
+    names[0] = "Zamboni";
+    System.out.println("\n\nbut elements can be changeds:");
+
+    for(String name : names) {
+      System.out.println(name);
+    }
+  }
+}
