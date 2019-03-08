69a7311a8a226e5dee9f11a6285a154ce6889715
Luiz Zamboni <luizamboni2002@hotmail.com>
2019-03-08 13:48:53 -0300

    ADD reactor example

--- 5-example-reactor/src/main/java/example/reactor/WebClients.java
+++ 5-example-reactor/src/main/java/example/reactor/WebClients.java
@@ -1,27 +1,35 @@
 package example.reactor;
 
 
-import io.netty.handler.codec.http.HttpResponseStatus;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.client.HttpClientResponse;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class WebClients {
 
     static String uri = "http://api.github.com/users/luizamboni/repos";
 
-    static Integer volume = 10;
+    static Integer volume = 100;
 
     public static void main(String[] args) {
 
-        syncHttpClient();
 
+//        System.out.println("sync single thread ---------------------------");
+//        syncHttpClient();
+
+        System.out.println("with thread in thread executor ---------------");
+        withThreads();
+
+
+        System.out.println("with reactor ---------------------------------");
         asyncHttpClient();
 
         while(true) {
@@ -31,6 +39,46 @@ public class WebClients {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void withThreads() {
+
+        Long initial = System.currentTimeMillis();
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        Callable<Boolean> task = () -> {
+            HttpClientResponse response = HttpClient.create()
+                    .get()
+                    .uri(uri)
+                    .response()
+                    .block();
+
+            System.out.println("http status: " + response.status());
+            System.out.println(Thread.currentThread().getName());
+
+            return true;
+        };
+
+        Collection<Callable<Boolean>> tasks = new ArrayList<>();
+
+        IntStream.range(0, volume).forEach(i -> {
+            tasks.add(task);
+        });
+
+
+        try {
+            executor.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        executor.shutdown();
+
+        System.out.println(
+            "enlapsed time: " + (System.currentTimeMillis() - initial) + " milliseconds"
+        );
     }
 
     public static void syncHttpClient(){
@@ -61,8 +109,9 @@ public class WebClients {
         Long initial = System.currentTimeMillis();
 
 
+
         Flux<HttpClientResponse> pooling = Flux.range(0, volume)
-                .publishOn(Schedulers.single())
+//                .publishOn(Schedulers.single())
 //                .log()
                 .flatMap(integer -> {
                     System.out.println(Thread.currentThread().getName());
@@ -70,7 +119,8 @@ public class WebClients {
                     Mono<HttpClientResponse> response = HttpClient.create()
                             .get()
                             .uri(uri)
-                            .response();
+                            .response()
+                            .publishOn(Schedulers.single());
 
                     return response;
 
